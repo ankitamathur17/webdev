c445157af12e0bbb82195a619d53e61b096cbfd4
Ankita Mathur <ankita17.am@gmail.com>
2019-12-22 23:00:08 +0530

    webpack-merge

---
+++ webpack/webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_dist'),
+    filename: 'india.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My website',
+      template:'./template/client.html'
+    }),
+  ],
+};
