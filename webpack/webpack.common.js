d193f13ff08a9342228413584993c2db97ed2b48
Ankita Mathur <ankita17.am@gmail.com>
2019-12-24 23:02:31 +0530

    visualizer and bundleanalyzer added

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Visualizer = require('webpack-visualizer-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
   entry: {
     app: './src/client/index.js',
@@ -27,5 +29,11 @@ module.exports = {
       template:'./template/client.html'
     }),
     new CleanWebpackPlugin(),
+    new Visualizer(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      reportFilename: 'bundle.html',
+    }),
   ],
 };
